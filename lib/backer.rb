e4b14f904b91167fc47fd9cf3b9a9a4a443c2971
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-14 22:47:20 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,15 @@
 class Backer
-  attr_accessor :backed_project
+  attr_accessor :backed_projects
   attr_reader :name
   
   def initialize(name)
     @name = name
-    @backed_project = []
+    @backed_projects = []
   end
   
   def back_project(title)
     project = Project.new(title)
-    @backed_project << project
+    @backed_projects << project
   end
   
 end
